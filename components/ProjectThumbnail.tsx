d1201e0d52a1509cf6820dffe2cd8a4433b81829
Daniel Kelly <daniel.brandon.kelly@gmail.com>
2020-05-06 20:29:40 -0400

    Add details to project thumbnail component

--- components/ProjectThumbnail.tsx
+++ components/ProjectThumbnail.tsx
@@ -1,17 +1,28 @@
-enum LanguageColors {
-  TypeScript = 'text-blue-400',
-  JavaScript = 'text-yellow-400',
-  HTML = 'text-red-500',
+const LanguageColors = {
+  TypeScript: 'text-blue-400',
+  JavaScript: 'text-yellow-400',
+  HTML: 'text-red-500',
+}
+
+// TODO: Move this into one file
+const safeLinkProps = {
+  target: '_blank',
+  rel: 'noopener noreferrer',
 }
 
 export default (props) => {
   return (
-    <div className="bg-gray-900 odd:bg-gray-800 w-full md:w-1/3 h-64 py-3 px-6">
+    <a
+      href={props.homepage || props.html_url}
+      {...safeLinkProps}
+      className="group relative bg-gray-900 odd:bg-gray-800 w-full md:w-1/3 h-64 py-3 px-6"
+    >
+      <div className="absolute top-0 left-0 h-1 bg-blue-400 transition-width duration-200 ease-in-out w-0 group-hover:w-full" />
       <div className="flex justify-between">
         <h3 className="text-white text-2xl">{props.name}</h3>
         <h3 className={`text-white text-xl ${LanguageColors[props.language]}`}>{props.language}</h3>
       </div>
       <p className="project-description text-white opacity-50">{props.description}</p>
-    </div>
+    </a>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   theme: {
     extend: {},
+    boxShadow: {
+      custom: 'inset -6px -4px rgba(255, 145, 0)',
+    },
   },
   variants: {
     backgroundColor: ['responsive', 'hover', 'focus', 'odd'],
     display: ['responsive', 'hover'],
+    width: ['responsive', 'group-hover'],
   },
   plugins: [],
 }
